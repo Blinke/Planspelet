975f51b3153cc676d096d54fc794037d8268825a
LukasDissing <lukas.dissing@gmail.com>
2014-04-24 11:03:59 +0200

    Bokvisualisering

---
+++ Planspelet/Archive.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Bookshelf
+{
+    class Archive
+    {
+        List<Book> books;
+        int rows;
+        int columns;
+        int numOfBooks;
+        int numOfShelves;
+        
+        Vector2 position;
+        float rowSpacing = 110;
+        float columnSpacing = 80;
+
+        int activeShelf = 0;
+        int selectionX = 0;
+        int selectionY = 0;
+
+        public Archive(int rows, int columns, Vector2 position)
+        {
+            books = new List<Book>();
+            this.rows = rows;
+            this.columns = columns;
+            numOfBooks = 0;
+            numOfShelves = 0;
+
+            this.position = position;
+        }
+
+        public void AddBook(Book book)
+        {
+            if (numOfBooks + 1 > rows * columns * numOfShelves) numOfShelves++;
+            books.Add(book);
+            numOfBooks++;
+        }
+
+        /// <summary>
+        /// Moves the selection-cursor in the given directions, the method only takes into account whether the parameters are positive, negative or zero. The selection only moves one step.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void MoveSelection(int x, int y)
+        {
+            if (x > 0) selectionX++;
+            else if (x < 0) selectionX--;
+            if (y > 0) selectionY++;
+            else if (y < 0) selectionY--;
+
+            int booksOnShelf = numOfBooks - rows * columns * (numOfShelves - 1);
+            int booksOnLastRow = booksOnShelf % columns;
+            int fullRows = booksOnShelf / columns;
+
+            if (x > 0)
+            {
+                if (selectionY == fullRows && selectionX > booksOnLastRow - 1)
+                {
+                    selectionX = 0;
+                    selectionY = 0;
+                }
+                else if (selectionX > columns - 1)
+                {
+                    selectionX = 0;
+                    selectionY++;
+                }
+            }
+            else if (x < 0)
+            {
+                if (selectionX < 0)
+                {
+                    if (selectionY == 0)
+                    {
+                        selectionX = booksOnLastRow - 1;
+                        selectionY = fullRows;
+                    }
+                    else
+                    {
+                        selectionX = columns - 1;
+                        selectionY--;
+                    }
+                }
+            }
+
+            if (y > 0)
+            {
+                if (selectionY > rows - 1)
+                selectionY = 0;
+                if (selectionY == fullRows && selectionX > booksOnLastRow - 1)
+                    selectionX = booksOnLastRow - 1;
+            }
+            else if (y < 0 && selectionY < 0)
+            {
+                selectionY = 0;
+                selectionX = 0;
+            }
+        }
+
+        public Book ReturnSelection()
+        {
+            int selection = activeShelf * selectionX * selectionY + selectionX + selectionY * columns;
+            return books[selection];
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            int counter = 0;
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < columns; x++)
+                {
+                    if (counter < books.Count)
+                    {
+                        if (x == selectionX && y == selectionY)
+                            books[counter].Draw(spriteBatch, position + new Vector2(columnSpacing * x, rowSpacing * y), Color.Yellow);
+                        else
+                            books[counter].Draw(spriteBatch, position + new Vector2(columnSpacing * x, rowSpacing * y), Color.White);
+                    }
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Planspelet/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Bookshelf
+{
+    class Book
+    {
+        Texture2D baseTexture;
+
+        public Book(Texture2D baseTexture)
+        {
+            this.baseTexture = baseTexture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, Color tint)
+        {
+            spriteBatch.Draw(baseTexture, position, tint);
+        }
+    }
+}
